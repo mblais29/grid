538ac68d20fe79d81e981c6e9773fe165c174bd4
mblais29 <mblais29@gmail.com>
2015-11-14 19:34:12 +0000

    - organized the code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 (function(){
-
+	//Application Modules
 	var app = angular.module('myApp',[]);
+
 	//style 0 == grid, style == 1 is list
 	var style = 0;
 	
+	//Application Controllers
 	app.controller('storeController', function($scope, $http) {
 	  
 	  this.products = bikes;
 
 	});
-	
+	//Temporary stores the Form information to Local Storage
 	app.controller('tempAdd', function($scope) {
 		
 		this.addItem = function($scope){
@@ -20,7 +22,7 @@
 		};
 	});
 		
-		
+	//Checks to see if the form is Visible or not	
 	app.controller('check', function($scope){
 		$scope.style = style;
 	    $scope.checkStyle = function() {
@@ -35,59 +37,56 @@
         
 	});
 	
-
-var bikes = [
-
-	{
-		"type": "Norco",
-		"price": "425",
-		"pic": "img/norco-mountain.png"
-		
-	},
-	{
-		"type": "Norco",
-		"price": "400",
-		"pic": "img/norco-mountain2.png"
-		
-	},
-	{
-		"type": "Norco",
-		"price": "530",
-		"pic": "img/norco-mountain3.png"
-		
-	},
-	{
-		"type": "Norco",
-		"price": "399",
-		"pic": "img/norco-cross.png"
-		
-	},
-	{
-		"type": "Kona",
-		"price": "629",
-		"pic": "img/kona-supreme_operator.jpg"
-		
-	},
-	{
-		"type": "Kona",
-		"price": "295",
-		"pic": "img/kona-process_167.jpg"
-		
-	},
-	{
-		"type": "Kona",
-		"price": "349",
-		"pic": "img/kona-hei_hei_supreme.jpg"
-		
-	},
-	{
-		"type": "Kona",
-		"price": "449",
-		"pic": "img/kona-rove_al.jpg"
-		
-	}
-];
-
+//JSON information 
+	var bikes = [
 	
-	
+		{
+			"type": "Norco",
+			"price": "425",
+			"pic": "img/norco-mountain.png"
+			
+		},
+		{
+			"type": "Norco",
+			"price": "400",
+			"pic": "img/norco-mountain2.png"
+			
+		},
+		{
+			"type": "Norco",
+			"price": "530",
+			"pic": "img/norco-mountain3.png"
+			
+		},
+		{
+			"type": "Norco",
+			"price": "399",
+			"pic": "img/norco-cross.png"
+			
+		},
+		{
+			"type": "Kona",
+			"price": "629",
+			"pic": "img/kona-supreme_operator.jpg"
+			
+		},
+		{
+			"type": "Kona",
+			"price": "295",
+			"pic": "img/kona-process_167.jpg"
+			
+		},
+		{
+			"type": "Kona",
+			"price": "349",
+			"pic": "img/kona-hei_hei_supreme.jpg"
+			
+		},
+		{
+			"type": "Kona",
+			"price": "449",
+			"pic": "img/kona-rove_al.jpg"
+			
+		}
+	];
 })();
